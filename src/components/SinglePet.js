0d29f45ede975fe71c641935370a0ce187646448
Finn <finnterdal@gmail.com>
2019-12-04 22:11:16 -0600

    install jsdom and configure it with mocha

--- src/components/SinglePet.js
+++ src/components/SinglePet.js
@@ -1,41 +1,10 @@
 import React from "react"
 
-
-// Trying out risky Babel features so that everything works.
-
-// Async Await
-async function hello() {
-  await Promise.resolve()
-  return "hello world"
-}
-hello()
-
-class Thing extends React.Component {
-  // Async class methods
-  async helpfulFn () {
-    await hello()
-    return "Thing"
-  }
-  render() {
-    // Array spreading
-    const things = [1, 2, 3, 4]
-    const [, , , last] = things
-    console.log(last)
-    return (
-      // Fragment shorthands
-      <>
-        <div>This is a Thing</div>
-      </>
-    )
-  }
-}
-
 const SinglePet = () => {
   return (
     <>
       <div className="single-pet">
         <h2>Rigatoni</h2>
-        <Thing />
         <div>Description: A flaming hot cheetoh in feline form</div>
         <hr />
         <div>Favorite foods:</div>
--- tests/tier-1.js
+++ tests/tier-1.js
@@ -1,10 +1,27 @@
 /* eslint-env mocha */
+import React from "react"
+import { render } from "@testing-library/react"
 
-import SinglePet from '../src/components/SinglePet'
+import SinglePet from "../src/components/SinglePet"
 
-describe("First Describe", () => {
-  it("Test 1", () => {})
+describe("SinglePet component", () => {
+  const rigatoni = {
+    name: "Rigatoni",
+    description: "A flaming hot cheetoh in feline form",
+    favoriteFoods: [
+      "Fancy Feast: Salmon Pâté",
+      "Fancy Feast: Chicken Liver Pâté",
+      "Tuna straight from the can",
+      "Shoelaces"
+    ]
+  }
+
+  it("renders the pet name passed in as props", async () => {
+    const { findAllByText } = render(<SinglePet pet={rigatoni} />)
+    const matching = await findAllByText("Rigatoni")
+    console.log('MATCHING', matching.length)
+  })
   it("Test 2", () => {
-    throw new Error("Bad things")
+    // throw new Error("Bad things")
   })
 })
