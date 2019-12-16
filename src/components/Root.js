2bf1c2558faec173b26f5b326aaa0a10a0f4ca00
Finn <finnterdal@gmail.com>
2019-12-16 14:15:23 -0600

    provides a sample express server and loads data into React with it

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from "react"
-import pets from "../petdata"
+import React, { useEffect, useState } from "react"
+import oldPets from "../petdata"
 import PetList from "./PetList"
-import axios from 'axios'
+import axios from "axios"
 
 const Root = () => {
+  const [pets, setPets] = useState([])
   useEffect(() => {
-    console.log('Retrieving data...')
+    console.log("Retrieving data...")
     ;(async function() {
-      const { data } = await axios.get('http://localhost:4321/api/pets')
+      const { data } = await axios.get("http://localhost:4321/api/pets")
       console.log(data)
+      setPets(data)
     })()
-  })
+    // If you don't provide a dependency array here, the component will
+    // re-render in an infinite loop
+  }, [])
   return (
     <>
       <h1>Adoption Center</h1>
