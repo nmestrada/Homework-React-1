710ee4311af5b73f7c9ff757654c9d2c9cb52a03
Finn <finnterdal@gmail.com>
2020-01-07 16:14:49 -0600

    class solution for PetList

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -3,41 +3,89 @@ import SinglePet from "./SinglePet"
 // import pets from "../petdata"
 // const [rigatoni] = pets
 
-const PetList = props => {
-  const [filter, setFilter] = useState("all")
-  const handleSelectChange = evt => {
-    setFilter(evt.target.value)
+// Class Solution
+class PetList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      filter: "all"
+    }
+    this.handleSelectChange = this.handleSelectChange.bind(this)
   }
-  const pets = props.pets.filter(pet => {
-    if (filter === "all") return pet
-    if (filter === "cats") return pet.species === "cat"
-    if (filter === "dogs") return pet.species === "dog"
-  })
-  return (
-    <>
-      {/* We've added a testid to this div so that we can easily find it
-          in the tests. */}
-      <div data-testid="species-filter">
-        <label htmlFor="speciesFilter">Filter by species: </label>
-        <select
-          onChange={handleSelectChange}
-          value={filter}
-          name="speciesFilter"
-        >
-          <option>all</option>
-          <option>cats</option>
-          <option>dogs</option>
-        </select>
-      </div>
-      <div className="pet-list">
-        {pets.map(pet => {
-          return <SinglePet key={pet.name} pet={pet} />
-        })}
-      </div>
-    </>
-  )
+  handleSelectChange(evt) {
+    this.setState({ filter: evt.target.value })
+  }
+  render() {
+    const { filter } = this.state
+
+    const pets = this.props.pets.filter(pet => {
+      if (filter === "all") return pet
+      if (filter === "cats") return pet.species === "cat"
+      if (filter === "dogs") return pet.species === "dog"
+    })
+    return (
+      <>
+        {/* We've added a testid to this div so that we can easily find it
+            in the tests. */}
+        <div data-testid="species-filter">
+          <label htmlFor="speciesFilter">Filter by species: </label>
+          <select
+            onChange={this.handleSelectChange}
+            value={filter}
+            name="speciesFilter"
+          >
+            <option>all</option>
+            <option>cats</option>
+            <option>dogs</option>
+          </select>
+        </div>
+        <div className="pet-list">
+          {pets.map(pet => {
+            return <SinglePet key={pet.name} pet={pet} />
+          })}
+        </div>
+      </>
+    )
+  }
 }
 
+// Hooks Solution
+// const PetList = props => {
+//   const [filter, setFilter] = useState("all")
+//   const handleSelectChange = evt => {
+//     setFilter(evt.target.value)
+//   }
+//   const pets = props.pets.filter(pet => {
+//     if (filter === "all") return pet
+//     if (filter === "cats") return pet.species === "cat"
+//     if (filter === "dogs") return pet.species === "dog"
+//   })
+//   return (
+//     <>
+//       {/* We've added a testid to this div so that we can easily find it
+//           in the tests. */}
+//       <div data-testid="species-filter">
+//         <label htmlFor="speciesFilter">Filter by species: </label>
+//         <select
+//           onChange={handleSelectChange}
+//           value={filter}
+//           name="speciesFilter"
+//         >
+//           <option>all</option>
+//           <option>cats</option>
+//           <option>dogs</option>
+//         </select>
+//       </div>
+//       <div className="pet-list">
+//         {pets.map(pet => {
+//           return <SinglePet key={pet.name} pet={pet} />
+//         })}
+//       </div>
+//     </>
+//   )
+// }
+
+// Possible starting state...
 // Currently, PetList renders one SinglePet. We'd like it to render a list of
 // pets, passed in as props.pets.
 // const PetList = () => {
