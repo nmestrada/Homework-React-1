5509b6394470d872638d78fc8461e1d5dfdef6c7
Finn <finnterdal@gmail.com>
2020-01-07 16:24:50 -0600

    class solution for SinglePet

--- src/components/SinglePet.js
+++ src/components/SinglePet.js
@@ -11,25 +11,60 @@ import React, { useState } from "react"
 }
 */
 
-const SinglePet = props => {
-  const { name, description, species } = props.pet
-  const [adopted, setAdopted] = useState(false)
-  return (
-    <div className="single-pet">
-      <h2>{name}</h2>
-      <div>
-        {species}
-        <span>
-          {species === "cat" && " 😸"}
-          {species === "dog" && " 🐶"}
-        </span>
+// Class Solution
+class SinglePet extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      adopted: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState(prevState => ({ adopted: !prevState.adopted }))
+  }
+  render() {
+    const { adopted } = this.state
+    const { name, description, species } = this.props.pet
+    return (
+      <div className="single-pet">
+        <h2>{name}</h2>
+        <div>
+          {species}
+          <span>
+            {species === "cat" && " 😸"}
+            {species === "dog" && " 🐶"}
+          </span>
+        </div>
+        <div>{description}</div>
+        <hr />
+        <div>{adopted ? "Adopted!" : "Available for adoption"}</div>
+        <button onClick={this.handleClick}>Toggle Adopted</button>
       </div>
-      <div>{description}</div>
-      <hr />
-      <div>{adopted ? "Adopted!" : "Available for adoption"}</div>
-      <button onClick={() => setAdopted(!adopted)}>Toggle Adopted</button>
-    </div>
-  )
+    )
+  }
 }
+
+// Hooks Solution
+// const SinglePet = props => {
+//   const { name, description, species } = props.pet
+//   const [adopted, setAdopted] = useState(false)
+//   return (
+//     <div className="single-pet">
+//       <h2>{name}</h2>
+//       <div>
+//         {species}
+//         <span>
+//           {species === "cat" && " 😸"}
+//           {species === "dog" && " 🐶"}
+//         </span>
+//       </div>
+//       <div>{description}</div>
+//       <hr />
+//       <div>{adopted ? "Adopted!" : "Available for adoption"}</div>
+//       <button onClick={() => setAdopted(!adopted)}>Toggle Adopted</button>
+//     </div>
+//   )
+// }
 
 export default SinglePet
