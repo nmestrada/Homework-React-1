c152fb7df5dc1177fe7839b4cba5be6aa7d91aa9
Finn <finnterdal@gmail.com>
2020-01-15 14:11:56 -0600

    got some delete button stuff working

--- src/components/DeletePet.js
+++ src/components/DeletePet.js
@@ -1,8 +1,20 @@
 import React from "react"
 import axios from "axios"
 
+/**
+ * Steps
+ * 1. Render a button with "Delete" in it
+ * 2. Give it the class "delete-button"
+ * 3. When user clicks the button, send DELETE to /api/pets/:petId
+ *    where petId is provided by props
+ * 4. Call the `handleDelete` function, provided on props
+ * 5. INTEGRATION STEP! The pet should now be deleted either
+ *    a. refetch the pets from the server again
+ *    b. syncronously remove the pet from the Root component
+ */
+
 const DeletePet = props => {
-  const { refetch, petId } = props
+  const { handleDelete, petId } = props
   const handleClick = async () => {
     try {
       await axios.delete(`/api/pets/${petId + 1}`)
