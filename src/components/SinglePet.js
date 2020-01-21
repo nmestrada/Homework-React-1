9ffc23a307829cc97389ac52f607070790da19da
Finn <finnterdal@gmail.com>
2020-01-21 16:35:55 -0600

    finish tier 1

--- src/components/SinglePet.js
+++ src/components/SinglePet.js
@@ -1,7 +1,30 @@
 import React from "react"
 
-const SinglePet = () => {
-  return <div className="single-pet"></div>
+class SinglePet extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      adopted: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState(prevState => ({ adopted: !prevState.adopted }))
+  }
+  render() {
+    const { name, description, species } = this.props.pet
+    const { adopted } = this.state
+    return (
+      <div className={`single-pet ${adopted ? "adopted" : ""}`}>
+        <div>{name}</div>
+        <div>{species}</div>
+        <div>{description}</div>
+        <hr />
+        <div>{adopted ? "Adopted!" : "Available"}</div>
+        <button onClick={this.handleClick}>Toggle Status</button>
+      </div>
+    )
+  }
 }
 
 export default SinglePet
