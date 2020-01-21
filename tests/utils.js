5e81829c588fa987039b27019d3098cf39bdf554
Finn <finnterdal@gmail.com>
2020-01-21 14:56:04 -0600

    clean up

--- tests/utils.js
+++ tests/utils.js
@@ -8,11 +8,3 @@ export const findButton = (wrapper, text) => {
     return node.type() === "button" && node.text() === text
   })
 }
-
-export const findFirstMatchingDiv = (wrapper, predicate = () => true) => {
-  const divs = wrapper.findWhere(node => {
-    return node.type() === "div" && predicate(node)
-  })
-  if (divs.length < 1) throw new Error("No matching divs found")
-  return divs.at(0)
-}
